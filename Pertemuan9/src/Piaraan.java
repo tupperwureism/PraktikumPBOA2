aa765b2bcbd4a2e6fedad20d34ab60caa46f0fc4
shalom kurniawan <trusukkendal@gmail.com>
2026-05-12 10:31:49 +0700

    Update Piaraan di Pertemuan 9 dengan tipe antrean

--- Pertemuan9/src/Piaraan.java
+++ Pertemuan9/src/Piaraan.java
@@ -1,6 +1,6 @@
 /*
 Nama : Shalom Kurniawan
-NIM ; 24060124120033
+NIM  : 24060124120033
 Kelas : A
 Tanggal : 4 Mei 2026
 */
@@ -8,81 +8,77 @@ Tanggal : 4 Mei 2026
 import java.util.*;
 
 public class Piaraan {
-    //atribut
+    // atribut
     private int nbelm;
-    private ArrayList<Anabul> Lanabul;
+    private Queue<Anabul> Lanabul; 
 
-    //method
-    public Piaraan(){
+    // method
+    public Piaraan() {
         nbelm = 0;
-        Lanabul = new ArrayList<>();
+        Lanabul = new LinkedList<>(); 
     }
 
-    public int getNbelm(){
+    public int getNbelm() {
         return nbelm;
     }
 
-    public void enqueueAnabul(Anabul A){
-        Lanabul.add(A);
+    public void enqueueAnabul(Anabul A) {
+        Lanabul.add(A); // add() di queue otomatis ngantri
         nbelm++;
     }
 
-    public boolean isMember(Anabul A){
-        for(Anabul B : Lanabul){
-            if(B == A){
-                return true;
-            }
-        }
-        return false;
+    public boolean isMember(Anabul A) {
+        return Lanabul.contains(A); 
     }
 
-    public Anabul getAnabul(){
-        return Lanabul.get(0);
+    public Anabul getAnabul() {
+        return Lanabul.peek(); //ngambil elemen paling awal
     }
 
-    public void dequeueAnabul(){
-        Anabul temp = getAnabul();
-        if(Lanabul.size() < 1){
+    public void dequeueAnabul() {
+        if (Lanabul.isEmpty()) {
             System.out.println("Belum punya piaraan");
             return;
         }
-        Lanabul.remove(0);
-        System.out.println("Anabul dengan nama " + temp.getPanggilan() + " dibuang");
+        
+        // poll() mengambil dan mengeluarkan elemen pertama dari antrean
+        Anabul temp = Lanabul.poll();
+        System.out.println("Anabul dengan nama " + temp.getNama() + " dibuang");
         System.out.println();
         nbelm--;
     }
 
-    public void showAnabul(){
-        for(Anabul A : Lanabul){
-            System.out.println(A.getPanggilan());
+    public void showAnabul() {
+        for (Anabul A : Lanabul) {
+            System.out.println(A.getNama());
             System.out.println();
         }
     }
 
-    public int countKucing(){
+    public int countKucing() {
         int temp = 0;
-        for(Anabul A : Lanabul){
-            if(A instanceof Kucing){
+        for (Anabul A : Lanabul) {
+            if (A instanceof Kucing) {
                 temp++;
             }
         }
         return temp;
     }
 
-    public int bobotKucing(){
-        int temp = 0;
-        for(Anabul A : Lanabul){
-            if(A instanceof Kucing){
-                temp += ((Kucing)A).getBobot();
+    public int bobotKucing() {
+        int temp = 0; // Menggunakan double agar sinkron dengan atribut bobot Kucing
+        for (Anabul A : Lanabul) {
+            if (A instanceof Kucing) {
+                temp += ((Kucing) A).getBobot();
             }
         }
         return temp;
     }
 
-    public void showJenisAnabul(){
-        for(Anabul A : Lanabul){
-            System.out.println(A.getPanggilan());
-            System.out.println(A.getClass());
+    public void showJenisAnabul() {
+        for (Anabul A : Lanabul) {
+            System.out.println(A.getNama());
+            System.out.println(A.getClass().getName()); 
             System.out.println();
         }
     }
